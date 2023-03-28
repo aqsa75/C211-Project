5dbb0a18166d3ae227f336e3d2e2ea4e902abc0e
hahnak <126526178+hahnak@users.noreply.github.com>
2023-03-27 21:37:57 -0400

    Add files via upload

---
+++ QuestionsAndAnswers.java
@@ -0,0 +1,15 @@
+public class QuestionsAndAnswers {
+    String[][] questions;
+
+    public QuestionsAndAnswers(String[][] questions) {
+        this.questions = questions;
+    }
+
+    public String[][] getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(String[][] questions) {
+        this.questions = questions;
+    }
+}
