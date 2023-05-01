e1ee7ac8c83a40535ef772e837118f4119c2012b
hahnak <126526178+hahnak@users.noreply.github.com>
2023-04-30 20:01:04 -0400

    Add files via upload
    Potential questions/answers to be added back into the main code

---
+++ questionsandanswers.java
@@ -0,0 +1,20 @@
+/*placeholder for question data to be added to main program
+
+public void initData(){
+    quiz[0][0] = “Would you rather:”;
+    quiz[0][1] = “Sunbathe on the beach”; quiz[0][2] = “Go skiing”;
+    quiz[1][0] = “Would you rather:”;
+    quiz[1][1] = “Go hiking in the trees”; quiz[1][2] = “swim in the ocean”;
+    quiz[2][0] = “Would you rather:”;
+    quiz[2][1] = “Go fishing in a lake”; quiz[2][2] = “walk along a sandy board walk”;
+    quiz[3][0] = “Would you rather:”;
+    quiz[3][1] = “Go to a cozy cabin”; quiz[3][2] = “stay in a high-end resort”;
+    quiz[4][0] = “Would you rather:”;
+    quiz[4][1] = “Wake up with birds chirping by your window”;
+    quiz[4][2] = “wake up with the sound of ocean waves coming to shore”;
+
+
+        }
+
+
+ */
